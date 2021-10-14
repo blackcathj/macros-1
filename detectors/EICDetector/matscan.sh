ce957aa12474200f3ff7740c85c48f24e7fdb15b
Jin Huang <jhuang@bnl.gov>
2021-10-14 12:09:17 -0400

    scan macro

---
+++ detectors/EICDetector/matscan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_base='data/Fun4All_G4_EICDetector_ECCE_Prop4_A_'
+
+for i in {0..1}
+do
+  
+  cmd="root -b -q Fun4All_G4_EICDetector.C($i) "
+  echo "=================================================================================================="
+  echo "$cmd ->  $file_base$i.log"
+  echo "=================================================================================================="
+  
+  $cmd  | tee $file_base$i.log
+  
+done
